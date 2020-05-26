a62c98870610b41bb5218e30796fca857e63b79a
Jotun Khan <gorshchyk@gmail.com>
2020-05-27 02:15:51 +0300

    update get_table; started encoding

--- find_index.py
+++ find_index.py
@@ -8,82 +8,42 @@ def find_the_page(street):
     try:
         int(finder)
         print("the first argument is an integer")
-        num = 1
     except ValueError:
         print('The first argument is not integer')
-        if finder.upper() == "А":
-            num = 2
-        elif finder.upper() == "Б":
-            num = 3
-        elif finder.upper() == "В":
-            num = 4
-        elif finder.upper() == "Г":
-            num = 5
-        elif finder.upper() == "Д":
-            num = 6
-        elif finder.upper() == "Е":
-            num = 7
-        elif finder.upper() == "Ж":
-            num = 8
-        elif finder.upper() == "З":
-            num = 9
-        elif finder.upper() == "И":
-            num = 10
-        elif finder.upper() == "К":
-            num = 11
-        elif finder.upper() == "Л":
-            num = 12
-        elif finder.upper() == "М":
-            num = 13
-        elif finder.upper() == "Н":
-            num = 14
-        elif finder.upper() == "О":
-            num = 15
-        elif finder.upper() == "П":
-            num = 16
-        elif finder.upper() == "Р":
-            num = 17
-        elif finder.upper() == "С":
-            num = 18
-        elif finder.upper() == "Т":
-            num = 19
-        elif finder.upper() == "У":
-            num = 20
-        elif finder.upper() == "Ф":
-            num = 21
-        elif finder.upper() == "Ц":
-            num = 22
-        elif finder.upper() == "Ч":
-            num = 23
-        elif finder.upper() == "Ш":
-            num = 24
-        elif finder.upper() == "Щ":
-            num = 25
-        elif finder.upper() == "Э":
-            num = 26
-        elif finder.upper() == "Ю":
-            num = 25
-        elif finder.upper() == "Я":
-            num = 26
+        alphabet = ["А", "Б", "В", "Г", "Д", "Е", "Ж", "З", "И", "К",
+                    "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф",
+                    "Ц", "Ч", "Ш", "Щ", "Э", "Ю", "Я"]
+        if finder.upper() in alphabet:
+            for i in range(0, 29):
+                if alphabet[i] == finder.upper():
+                    num = i + 2
         else:
+            print("This street does not exist")
             num = 0
     finally:
         return num
 
 
-example1 = find_the_page("Ломоносова")
-example2 = find_the_page("8-го марта")
+example = find_the_page("Малевич")
+example2 = find_the_page("Валевич")
+example1 = find_the_page("Калевич")
 
 
-def parse_the_page(num):
+def get_table_on_page(num):
     if num == 0:
         return "Index not found, please enter the streetname using the cyrillic alphabet"
     else:
         kyiv_index_url = "http://indexua.net/kievzipindex/" + str(num) + "/"
         response = requests.get(kyiv_index_url)
         html_text = response.text
-        soup = BeautifulSoup(html_text, 'html.parser', from_encoding="utf-8")
+
+        soup = BeautifulSoup(html_text, 'html.parser')
+        table = soup.find("div", class_="content")
+        print(soup.title.string.encode("utf-8"))
+
+        return table
 
 
-parse_the_page(example1)
-parse_the_page(example2)
+get_table_on_page(example)
+get_table_on_page(example1)
+get_table_on_page(example2)
